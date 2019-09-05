af46d2d7cc179f9e2b553ccb0520d0c5abeb078c
twhalen <thor@otosense.com>
2019-09-05 11:11:23 -0700

    made sklearn save constructed learner instead of just displaying.

--- py2dash/scrap/sklearn_estimators.py
+++ py2dash/scrap/sklearn_estimators.py
@@ -6,13 +6,34 @@ def stringify_output(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
         return str(func(*args, **kwargs))
+
     return wrapped
 
 
-funcs = [stringify_output(x[1]) for x in all_estimators()][:70]
+def pickle_output(func):
+    import os
+    import pickle
+    func_name = func.__name__
+    save_filepath = os.path.expanduser(f"~/{func_name}.p")
+
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        obj = func(*args, **kwargs)
+        pickle.dump(obj, open(save_filepath, 'wb'))
+        return f"{str(obj)}\nSaved here: {save_filepath}"
+
+    return wrapped
+
+
+# choose here:
+# oputput_decorator = stringify_output
+oputput_decorator = pickle_output
+
+funcs = [oputput_decorator(x[1]) for x in all_estimators()][:10]
 
 if __name__ == '__main__':
     from py2dash.app_makers import dispatch_funcs
+
     print("**********************************************************")
     print(f"Dispatching {len(funcs)} functions. (Note that it takes about 1-3s per 10 functions to dispatch, "
           "so might have to wait sometimes.")
