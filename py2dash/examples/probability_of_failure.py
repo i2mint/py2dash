8626d1206a20d65d099df799720bbc5ed79328f9
twhalen <thor@otosense.com>
2019-09-17 09:23:58 -0700

    examples/probability_of_failure: Simple app for survival analysis calculations

--- py2dash/examples/probability_of_failure.py
+++ py2dash/examples/probability_of_failure.py
@@ -11,12 +11,20 @@ def any_failure_prob_for(n_units=DFLT_N_UNITS, unit_failure_prob=DFLT_UNIT_FAILU
     return 1 - ((1 - unit_failure_prob) ** n_units)
 
 
-def unit_failure_prob_for(n_units=DFLT_N_UNITS, any_failure_prob=DFLT_ANY_FAILURE_PROB):
-    return 1 - (1 - any_failure_prob) ** (1 / n_units)
-
-
 def n_units_for(any_failure_prob=DFLT_ANY_FAILURE_PROB, unit_failure_prob=DFLT_UNIT_FAILURE_PROB):
+    """n_units for which we get a given 'any failure' probability for a given unit failure probability
+    This can be useful if we want to know how many units we need to monitor if we want to be
+    `any_failure_prob` sure to observe at least one failure.
+    """
     return log(1 - any_failure_prob) / log(1 - unit_failure_prob)
+
+
+def unit_failure_prob_for(n_units=DFLT_N_UNITS, any_failure_prob=DFLT_ANY_FAILURE_PROB):
+    """ If we know the 'any failure' probability, and the number of units: What's the unit failure probability?
+    This can be useful if the data we have is the proportion of times there's at least one failure in a fixed-size
+    period, and we want to know what the unit failure probability is.
+    """
+    return 1 - (1 - any_failure_prob) ** (1 / n_units)
 
 
 funcs = [any_failure_prob_for, unit_failure_prob_for, n_units_for]
---
+++ py2dash/scrap/multiple_inputs_graph_output.py
@@ -0,0 +1,121 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'cb5392c35661370d95f300086accea51/raw/'
+    '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
+    'indicators.csv')
+
+n = 10000
+rand_base = lambda: np.random.randint(0, 10, n)
+
+
+indicator_name = 'Indicator Name'
+
+df = pd.DataFrame({indicator_name: np.random.choice(['A', 'B', 'C'], n),
+                   'Country Name': np.random.choice(['xxxxx', 'yyyy'], n),
+                   'Year': np.random.randint(0, 10, n) * 10,
+                   'Value': np.random.randint(0, 500, n) * 1
+                   })
+
+available_indicators = df[indicator_name].unique()
+dflt_indicator_val_0 = available_indicators[0]
+dflt_indicator_val_1 = available_indicators[1]
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value=dflt_indicator_val_0
+            ),
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value=dflt_indicator_val_1
+            ),
+            dcc.RadioItems(
+                id='yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        id='year--slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()},
+        step=None
+    )
+])
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    [Input('xaxis-column', 'value'),
+     Input('yaxis-column', 'value'),
+     Input('xaxis-type', 'value'),
+     Input('yaxis-type', 'value'),
+     Input('year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df[df['Year'] == year_value]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff[indicator_name] == xaxis_column_name]['Value'],
+            y=dff[dff[indicator_name] == yaxis_column_name]['Value'],
+            text=dff[dff[indicator_name] == yaxis_column_name]['Country Name'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
